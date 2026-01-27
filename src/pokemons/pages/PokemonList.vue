644d07f28d1462508f5d4fd0973be108a3c13999
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-01-27 13:14:54 -0400

    feat: Add axios dependency and implement Pokémon API integration

--- src/pokemons/pages/PokemonList.vue
+++ src/pokemons/pages/PokemonList.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+import pokemonApi from '@/api/pokemonApi'
 
+pokemonApi
+  .get('/pokemon?limit=10')
+  .then((response) => {
+    console.log(response.data)
+  })
+  .catch((error) => {
+    console.error('Error fetching Pokémon data:', error)
+  })
 </script>
 
 <template>
@@ -7,4 +16,3 @@
     <h1>Pokemon List</h1>
   </div>
 </template>
-
