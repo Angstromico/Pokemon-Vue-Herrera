ebea66ab496790b4b6f417e90f779f483a33f6a3
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-07 19:44:43 -0400

    refactor: Clean up unused API calls and improve Pokémon fetching logic

--- src/pokemons/pages/PokemonList.vue
+++ src/pokemons/pages/PokemonList.vue
@@ -1,30 +1,34 @@
 <script setup lang="ts">
-import pokemonApi from '@/pokemons/api/pokemonApi'
+import { ref } from 'vue'
+//import pokemonApi from '@/pokemons/api/pokemonApi'
 import type { IPokemonListResponse } from '@/pokemons/interfaces/pokemon-list.response'
-import type { IPokemonDataResponse } from '@/pokemons/interfaces/pokemon-data.response'
+//import type { IPokemonDataResponse } from '@/pokemons/interfaces/pokemon-data.response'
 import { getPokemons } from '../helpers/get-pokemons'
 
-pokemonApi
-  .get<IPokemonListResponse>('/pokemon?limit=10')
-  .then((response) => {
-    console.log(response.data.results[0].name.toUpperCase())
-  })
-  .catch((error) => {
-    console.error('Error fetching Pokémon data:', error)
-  })
+const pokemons = ref<IPokemonListResponse | null>(null)
 
-pokemonApi
-  .get<IPokemonDataResponse>('/pokemon/1')
-  .then((response) => {
-    console.log(response.data.abilities)
-  })
-  .catch((error) => {
-    console.error('Error fetching Pokémon data:', error)
-  })
+// pokemonApi
+//   .get<IPokemonListResponse>('/pokemon?limit=10')
+//   .then((response) => {
+//     console.log(response.data.results[0].name.toUpperCase())
+//   })
+//   .catch((error) => {
+//     console.error('Error fetching Pokémon data:', error)
+//   })
+
+// pokemonApi
+//   .get<IPokemonDataResponse>('/pokemon/1')
+//   .then((response) => {
+//     console.log(response.data.abilities)
+//   })
+//   .catch((error) => {
+//     console.error('Error fetching Pokémon data:', error)
+//   })
 
 getPokemons(10)
-  .then((pokemons: any) => {
+  .then((pokes: any) => {
     console.log(` Pokémon List:`, pokemons)
+    pokemons.value = pokes
   })
   .catch((error: any) => {
     console.error('Error fetching Pokémon data:', error)
