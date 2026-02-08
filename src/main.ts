5ddedb7755fc6fa8919e28b9e71d7f6b5518b22e
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-08 11:54:10 -0400

    refactor: Remove invalid 'cacheTime' property from VueQueryPlugin configuration

--- src/main.ts
+++ src/main.ts
@@ -15,12 +15,6 @@ VueQueryPlugin.install(app, {
         refetchOnWindowFocus: false,
         retry: 1,
         gcTime: 1000 * 60 * 10, // 10 minutes
-        /* Object literal may only specify known properties, and 'cacheTime' does not exist in type 'OmitKeyof<QueryObserverOptions<unknown, Error, unknown, unknown, readonly unknown[], never>, "queryKey"> & ShallowOption'.ts(2353)
-⚠ Error (TS2353)  |  |  | 
-
-Object literal may only specify known properties, and cacheTime does not exist in type:
-.
-(property) cacheTime: number */
         refetchOnReconnect: 'always'
       }
     }
