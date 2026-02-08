d50167f99ecb5d62fafc776cb11e0a04aa39f207
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-08 11:53:36 -0400

    refactor: Add @tanstack/vue-query dependency and configure VueQueryPlugin in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,29 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App)
 
+//app.use(VueQueryPlugin)
+VueQueryPlugin.install(app, {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: 1,
+        gcTime: 1000 * 60 * 10, // 10 minutes
+        /* Object literal may only specify known properties, and 'cacheTime' does not exist in type 'OmitKeyof<QueryObserverOptions<unknown, Error, unknown, unknown, readonly unknown[], never>, "queryKey"> & ShallowOption'.ts(2353)
+⚠ Error (TS2353)  |  |  | 
+
+Object literal may only specify known properties, and cacheTime does not exist in type:
+.
+(property) cacheTime: number */
+        refetchOnReconnect: 'always'
+      }
+    }
+  }
+}) // Use the install method to register the plugin
 app.use(router)
 
 app.mount('#app')
