7535eea9da7df37dbcff1b0eb28eb9fb003908c3
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-02-08 11:36:14 -0400

    refactor: Restructure Pokémon fetching logic in usePokemons to use async/await and improve loading state management

--- src/pokemons/composables/usePokemons.ts
+++ src/pokemons/composables/usePokemons.ts
@@ -1,19 +1,38 @@
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { getPokemons } from '@/pokemons/helpers'
 import type { IPokemonDataResponse } from '@/pokemons/interfaces'
 
-export const usePokemons = () => {
-  const pokemons = ref<IPokemonDataResponse[]>([])
-  const isLoading = ref(true)
+const pokemons = ref<IPokemonDataResponse[]>([])
+const isLoading = ref(true)
 
-  getPokemons(10)
-    .then((pokes: any) => {
+export const usePokemons = () => {
+  // if (pokemons.value.length === 0) {
+  //   getPokemons(10)
+  //     .then((pokes: any) => {
+  //       pokemons.value = pokes
+  //       isLoading.value = false
+  //     })
+  //     .catch((error: any) => {
+  //       console.error('Error fetching Pokémon data:', error)
+  //     })
+  // }
+
+  const fetchPokemons = async (limit: number) => {
+    try {
+      const pokes = (await getPokemons(limit)) as IPokemonDataResponse[]
       pokemons.value = pokes
-      isLoading.value = false
-    })
-    .catch((error: any) => {
+    } catch (error) {
       console.error('Error fetching Pokémon data:', error)
-    })
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  onMounted(async () => {
+    if (pokemons.value.length === 0) {
+      await fetchPokemons(10)
+    }
+  })
 
   return {
     pokemons,
